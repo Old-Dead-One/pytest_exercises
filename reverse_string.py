a8ca7393bf70cd7f6440ddb6523ef54ba2373dcd
Alan McGraw <alanmcgraw@proton.me>
2024-04-15 09:02:34 -0600

    Simple pytest to reverse a string

---
+++ reverse_string.py
@@ -0,0 +1,12 @@
+# Make a function that takes in a string and returns a reversed copy og the same string.
+
+# Function to reverse a string
+
+#print (s[::-1])
+
+
+def reverse_string(s):
+    return (s[::-1])
+
+#print (reverse_string(s))
+
---
+++ test_reverse_string.py
@@ -0,0 +1,9 @@
+from reverse_string import reverse_string
+
+
+def test_reverse_string():
+    s = "Hello"
+    r = "olleH"
+    assert reverse_string(s) == r
+
+    
